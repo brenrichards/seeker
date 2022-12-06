68add06c9eb14a188fb3c0b2f250b04758b68b03
Brendan Richards <brenrichards@byui.edu>
2022-12-05 17:07:07 -0700

    push

---
+++ game/seeker.py
@@ -0,0 +1,40 @@
+
+import random
+# 1) Add the class declaration. Use the following class comment.
+
+"""The person looking for the Hider. 
+
+The responsibility of a Seeker is to keep track of its location and distance travelled.
+
+Attributes:
+    location (int): The location of the Seeker (1-1000).
+"""
+class Seeker:
+# 2) Create the class constructor. Use the following method comment.
+    """Constructs a new Seeker.
+
+    Args:
+        self (Seeker): An instance of Seeker.
+    """
+    def __init__(self):
+        self._location = random.randint(1, 1000) 
+    
+# 3) Create the get_location(self) method. Use the following method comment.
+    """Gets the current location.
+    
+    Returns:
+        number: The current location,
+    """
+    def get_location(self):
+        return self._location
+
+    
+# 4) Create the move_location(self, location) method. Use the following method comment.
+    """Moves to the given location.
+
+    Args:
+        self (Seeker): An instance of Seeker.
+        location (int): The given location.
+    """
+    def move_location(self, location):
+        self._location = location
